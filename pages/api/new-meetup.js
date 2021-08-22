20052422661d903c8f783f9acf2d6eea5a971950
Itamar <itamarone@gmail.com>
2021-08-22 11:59:59 +0300

    added api

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,10 +1,27 @@
 // api/new-meetup
 
-const handler = (req, res) => {
+import { MongoClient } from "mongodb";
+require("dotenv").config();
+const handler = async (req, res) => {
   if (req.method === "POST") {
     const data = req.body;
 
     const { title, image, address, description } = data;
+
+    const client = await MongoClient.connect(
+      `mongodb+srv://Itamar:${process.env.MONGO_DB_PASSWORD}@next-js-tutorial.cs4ra.mongodb.net/meetupsDataBase?retryWrites=true&w=majority`
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup Inserted!" });
   }
 };
 
